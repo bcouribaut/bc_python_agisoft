9f9972d825f1f69f86d3cd7f016b6ebef039e13d
Bastien <28714262+bcouribaut@users.noreply.github.com>
2017-07-27 11:53:21 +0200

    script_1_chunk.py modifié

--- script_1_chunk.py
+++ script_1_chunk.py
@@ -10,34 +10,34 @@ chunk1 = doc.chunks[0]
 
 
 ### Photo Alignement ###
-chunk1.matchPhotos(PhotoScan.HighAccuracy)
+chunk1.matchPhotos(accuracy = PhotoScan.HighAccuracy, preselection=PhotoScan.NoPreselection, reference_preselection=True, filter_mask=False, keypoint_limit=40000, tiepoint_limit=4000)
 chunk1.alignCameras()
 
 doc.save()
 
 ### DenseCloud ###
-chunk1.buildDenseCloud(PhotoScan.MediumQuality, PhotoScan.AggressiveFiltering)
+chunk1.buildDenseCloud(quality=PhotoScan.MediumQuality, filter=PhotoScan.AggressiveFiltering)
 
 doc.save()
 
 ### Mesh ###
-#chunk1.buildModel(PhotoScan.Arbitrary, face_count=PhotoScan.MediumFaceCount)
+chunk1.buildModel(surface=PhotoScan.Arbitrary, source=PhotoScan.DenseCloudData, face_count=PhotoScan.MediumFaceCount, interpolation=PhotoScan.EnabledInterpolation)
 
 #doc.save()
 
 ### Texture ###
-#chunk1.buildUV(PhotoScan.GenericMapping, 2)
-#chunk1.buildTexture(PhotoScan.MosaicBlending, False, 8192, True)
+chunk1.buildUV(mapping=PhotoScan.GenericMapping, count=2)
+chunk1.buildTexture(blending=PhotoScan.MosaicBlending, color_correction=False, size=8192, fill_holes=True)
 
 #doc.save()
 
 ### DEM ###
-#chunk1.buildDem(PhotoScan.DenseCloudData)
+chunk1.buildDem(PhotoScan.DenseCloudData)
 
 #doc.save()
 
 ### Orthomosaic ###
-#chunk1.buildOrthomosaic(PhotoScan.ElevationData, PhotoScan.MosaicBlending)
+chunk1.buildOrthomosaic(surface=PhotoScan.ElevationData, blending=PhotoScan.MosaicBlending, color_correction=False, fill_holes=True)
 
 #doc.save()
 
